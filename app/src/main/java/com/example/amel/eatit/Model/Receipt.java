c2dfb892bacf4e0743ac6ff633601be24614a946
ameljbeli <amel.jbeli@yahoo.fr>
2018-12-27 16:36:37 +0100

    first commit

---
+++ app/src/main/java/com/example/amel/eatit/Model/Receipt.java
@@ -0,0 +1,12 @@
+package com.example.amel.eatit.Model;
+
+import java.util.List;
+
+/**
+ * Created by 123456 on 2017/11/27.
+ */
+
+public class Receipt {
+    public List<Order> items;
+    public String totalcost;
+}
